989434ce6d78997d32ac8bd601fb2fda8abb616f
Torok Andras <torok.andras@hotmail.com>
2020-08-21 21:51:41 +0200

    szamolas js-ben

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-// console.log("Hello, this is a console message");
-
-
+function calcAmount() {
+    let price = 1000;
+    let amountInput = document.querySelector("input[name='amount-input']")
+    let showAmount = document.querySelector("span.show-amount")
+    let amount = parseInt(amountInput.value) * price;
+    
+    showAmount.innerHTML = amount;
+}
