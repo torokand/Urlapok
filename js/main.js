9156b390e104c47d96751bb81ff00405f6787b77
Torok Andras <torok.andras@hotmail.com>
2020-08-26 19:13:39 +0200

    Events

--- js/main.js
+++ js/main.js
@@ -23,3 +23,54 @@ function showSumPrice(pricePerProduct = 1000, productNumber = 1) {
     }
 
 }
+
+// Add helptext.
+let helpText = document.createElement("small");
+helpText.className = "form-text text-muted";
+helpText.innerHTML = "Adja meg a feltéteket!";
+// console.log( helpText );
+
+let parent = document.querySelector("div.form-group:nth-child(1)");
+parent.appendChild(helpText);
+
+// parent.removeChild(helpText);
+
+//Window events.
+let sendButton = document.querySelector("form .btn.btn-primary");
+/*    sendButton.onclick = function() {
+// vagysendButton.onclick = calcAmount; 
+   alert("Hello JS!");
+};
+*/
+
+
+
+// let sendButton = document.querySelector("form.btn.btn-primary");
+/* sendButton.addEventListener("click", function() {
+    alert("Hello JS!");
+}); */
+
+
+window.addEventListener("resize", function() {
+    console.log(this.innerHeight, this.innerWidth);
+    });
+
+// Űrlap események.
+
+let orderForm = document.querySelector("#orderForm");
+orderForm.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+    console.log( this );
+    
+    let inputs = this.querySelectorAll("input");
+    let values = {};
+    for (let i=0; i < inputs.length; i++) {
+        values[inputs[i].name] = inputs[i].value;
+    }
+    console.log( values );
+});
+
+
+/*     */
+
+
